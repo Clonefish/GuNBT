72f1bc836671a206a7773733222a06651c798146
Dan Printzell <wildn00b@softwepz.co.cc>
2012-05-14 21:24:28 +0200

    Added base Signed-off-by: Dan Printzell <wildn00b@softwepz.co.cc>

---
+++ GuNBT.c
@@ -0,0 +1,27 @@
+#include "GuNBT.h"
+#define GUNBT_H
+
+void gnbt_parse_file(FILE * fp)
+{
+
+}
+
+void gnbt_save_tree(nbt_node * tree, const char * path)
+{
+
+}
+
+void gnbt_free(nbt_node)
+{
+
+}
+
+gnbt_node * nbt_find_by_name(nbt_node * tree, const char * name)
+{
+
+}
+
+gnbt_node * nbt_add_to_compound(nbt_node * tree, nbt_node * node)
+{
+
+}
--- GuNBT.h
+++ GuNBT.h
@@ -21,9 +21,9 @@ typedef struct gnbt_node {
     } gnbt_payload;
 } gnbt_node;
 
-gnbt_parse_file(FILE * fp);
-gnbt_save_tree(nbt_node * tree, const char * path);
-gnbt_free(nbt_node);
+void gnbt_parse_file(FILE * fp);
+void gnbt_save_tree(nbt_node * tree, const char * path);
+void gnbt_free(nbt_node);
 gnbt_node * nbt_find_by_name(nbt_node * tree, const char * name);
 gnbt_node * nbt_add_to_compound(nbt_node * tree, nbt_node * node);
 #endif
