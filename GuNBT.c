5b313be810e3abe63dd0eff6515fe3df0027fb84
Dan Printzell <wildn00b@softwepz.co.cc>
2012-05-15 17:25:25 +0200

    Added base, will try to pull the new code from main to the NBT branch Signed-off-by: Dan Printzell <wildn00b@softwepz.co.cc>

--- GuNBT.c
+++ GuNBT.c
@@ -1,8 +1,66 @@
 #include "GuNBT.h"
 #include <errno.h>
+#define null 0
+
+char * gnbt_read_name(unsigned char * stream)
+{
+    char *
+}
+
+gnbt_node * gnbt_read_node(unsigned char * stream)
+{
+    gnbt_node * node = malloc(sizeof(gnbt_node));
+    node->type = *++stream;
+    node->name = (node->type != gnbt_type_end) ? gnbt_readname(stream) : "END";
+    switch (node->type)
+    {
+        case gnbt_type_end:
+            return node;
+        case gnbt_type_byte:
+            node->data->tag_byte = *++stream;
+            break;
+        case gnbt_type_short:
+            node->data->tag_short = *stream;
+            stream += 2;
+            break;
+        case gnbt_type_int:
+            node->data->tag_int = *stream;
+            stream += 4;
+            break;
+        case gnbt_type_long:
+            node->data->tag_long = *stream;
+            stream += 8;
+            break;
+        case gnbt_type_float:
+            node->data->tag_float = *stream;
+            stream += 4;
+            break;
+        case gnbt_type_double:
+            node->data->tag_double = *stream;
+            stream += 8;
+            break;
+        case gnbt_type_byte_array:
+            stream += gnbt_read_byte_array(node, stream);
+            break;
+        case gnbt_type_string:
+            stream += gnbt_read_string(node, stream);
+            break;
+        case gnbt_type_list:
+            stream += gnbt_read_list(node, stream);
+            break;
+        case gnbt_type_short:
+            stream += gnbt_read_int_array(node, stream);
+            break;
+
+    }
+    return node;
+}
+
 
 void gnbt_parse_file(FILE * fp)
 {
+    unsigned char * stream = null;
+    gnbt_node * tree = gnbt_readnode(stream);
 
 }
 
